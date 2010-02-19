9dcef496711ecd6ae466d5dbd0b347a3fbce5201
John Tiernan <jtiernan@drownedmusic.co.uk>
2010-02-19 22:52:06 +0000

    second commit, initial structure

--- MPGCalc.java
+++ MPGCalc.java
@@ -5,6 +5,7 @@ public class MPGCalc
 {
 	public static void main(String args[])
 	{
-
+		System.out.println("MPG Calculator\nJohn Tiernan & co\n19-2-2010");
+		System.out.println("Initialising...\n\tWARNING: This is the command line version");
 	}
 }
